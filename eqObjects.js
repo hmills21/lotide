005ee21a09b5c3e30280b5435df0165065f09132
hmills21 <hailemillss@gmail.com>
2023-05-28 13:32:16 -0600

    finished eqObjects function assignment

---
+++ eqObjects.js
@@ -0,0 +1,54 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(` 👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  }   else if (actual !== expected) {
+    console.log(` 🤡🤡🤡 Assertion Failed: ${actual}!== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } for (let i = 0; i <= arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  const objKeys1 = Object.keys(object1);
+  const objKeys2 = Object.keys(object2);
+  if (objKeys1.length !== objKeys2.length) {
+    return false;
+  }
+  for (let item of objKeys1) {
+    if (Array.isArray(object1[item]) && Array.isArray(object2[item])) {
+      if (eqArrays(object1[item], object2[item]) !== true) {
+        return false;
+      }
+    } else {
+      if (object1[item] !== object2[item]) { // comparing the values of the keys of both objects
+        return false;
+      }
+    }
+  }
+
+
+  return true;
+};
+
+/*const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+assertEqual(eqObjects(shirtObject, anotherShirtObject), false); // => true
+
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+assertEqual(eqObjects(shirtObject , longSleeveShirtObject)); // => false*/
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+assertEqual(eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject), true); // => true
+
+const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+assertEqual(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject), false); // => false
